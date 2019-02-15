e9026272595d47c0666426e28f8f2a37327e232a
Nissan Holzer <nissan.holzer@gmail.com>
2019-02-15 00:39:37 +0000

    Done.

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,12 @@
+class MP3Importer
+  attr_reader :path, :files
+  
+  def initialize(file_path)
+    @path = file_path
+    @files << file_path
+  end 
+  
+  def files 
+    @files = []
+  end 
+end 
