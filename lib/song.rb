0f8de938e323aed1b9f4986dcb6dbd3ccbcc5a82
Nissan Holzer <nissan.holzer@gmail.com>
2019-02-15 01:39:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,9 +9,9 @@ class Song
   
   def new_by_filename(filename)
     file = filename.split(" - ")
-    song_name = file[1]
+    song = file[1]
     artist = file[0]
-    song_new = Song.new(song_name)
+    song_new = Song.new(song)
     song_new.artist = artist
   end 
 end 
