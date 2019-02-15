d580ea4ff18a99b3872b8c002a474ac87c546940
Nissan Holzer <nissan.holzer@gmail.com>
2019-02-15 02:18:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,8 +16,8 @@ class Song
     song
   end 
   
-  def artist_name 
-    self.artist = Artist.find_or_create_by_name(self)
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
     self.artist.add_song(self)
   end 
   
