b883497d805a7c7bf691637a7f19127f0199ccec
Nissan Holzer <nissan.holzer@gmail.com>
2019-02-15 01:26:38 +0000

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,11 +1,17 @@
+require 'pry'
+
 class MP3Importer
-  attr_reader :path, :files
+  attr_accessor :path, :files
   
   def initialize(file_path)
     @path = file_path
   end 
   
   def files 
-    @files = []
+    Dir.entries(@path).select {|entry| entry.include?(".mp3")}
+  end 
+  
+  def import 
+    
   end 
 end 
